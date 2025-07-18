94318b33d0b2a17861e9db05e77e934ff08185ea
Mariano Pisano <mapsyserick@hotmail.com>
2025-07-18 14:14:42 -0300

    env variable

--- src/dbo/config.js
+++ src/dbo/config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://mapsys2007:phut5lE2TWOCoru7@cluster0.jammggy.mongodb.net/tienda?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("✅ Conexión a MongoDB exitosa");
   } catch (error) {
     console.error("❌ Error al conectar a MongoDB:", error.message);
